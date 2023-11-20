0778949b3a94f5e4f47c546183a54dcacd325219
borg93 <gabriel.borg93@gmail.com>
2023-11-20 10:56:25 +0100

    test strucutre

--- src/htrflow/models/base_model.py
+++ src/htrflow/models/base_model.py
@@ -2,6 +2,17 @@ from abc import ABC, abstractmethod
 
 
 class BaseModel(ABC):
+
+    def __init__(self):
+        self._model
+        self.framework
+        self.device
+
     @abstractmethod
     def predict(self):
         pass
+
+    @property
+    @abstractmethod
+    def model(self):
+        return self._model
--- src/htrflow/structures/data_loader.py
+++ src/htrflow/structures/data_loader.py
@@ -1,6 +1,5 @@
 import cv2
 
-
 # TODO: add rapids.ai
 
 
@@ -28,11 +27,47 @@ class DataLoader:
         pass
 
 
+
 if __name__ == "__main__":
-    import pandas as pd
 
-    temp_data_structures_df = pd.DataFrame(columns=["Filename", "Filepath"])
+    import os
+    from imghdr import what
 
-    generator = DataLoader(temp_data_structures_df)
-    for image_batch in generator.image_batch_loader(batch_size=10):
-        ...  # process image batch
+    from datasets import Dataset, Image
+
+
+    image_folder = "/home/gabriel/Desktop/htrflow_core/data/raw"
+
+    images_path_list = []
+    for image in os.listdir(image_folder):
+        full_path = os.path.join(image_folder, image)
+        if what(full_path):
+            images_path_list.append(full_path)
+
+    # print(images_path_list)
+
+    dataset = Dataset.from_dict({"image": images_path_list}).cast_column("image", Image())
+    print(dataset[0]["image"])
+
+    numpy_dataset = dataset.with_format("numpy")
+
+    print(numpy_dataset[0]["image"])
+
+    # dataloader = DataLoader(numpy_dataset, batch_size=3)
+
+
+    # for image in numpy_dataset:
+    #     print(image)
+
+    print(numpy_dataset.to_pandas())
+
+
+
+
+    # import pandas as pd
+
+    # temp_data_structures_df = pd.DataFrame(columns=["Filename", "Filepath"])
+
+    # generator = DataLoader(temp_data_structures_df)
+    # for image_batch in generator.image_batch_loader(batch_size=10):
+    #     ...  # process image batch
