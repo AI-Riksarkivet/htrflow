AI-Riksarkivet/htrflow

commit 5873af293dc17d8c1231c9d9021873c5694eb846

Clean volume.py and update tests
Tried to solve some mypy complaints in volume.py, which resulted in
splitting the Node class into two: One base class for purely tree-
related stuff (traversal, printing, etc) and a extender class which acts
as a shared interface for RegionNode and PageNode.

Updated the tests accordingly and fixed a bug in the serialization tests.

src/htrflow_core/volume.py
@@ -3,39 +3,31 @@ This module holds the base data structures
 """
 
 import os
-from functools import singledispatchmethod
-from typing import Callable, Literal, Optional
+from abc import ABC, abstractmethod, abstractproperty
+from collections import namedtuple
+from itertools import chain
+from typing import Callable, Literal, Optional, Sequence
 
 import cv2
-import numpy as np
 
 from htrflow_core import image, serialization
 from htrflow_core.results import RecognitionResult, Segment, SegmentationResult
 
 
-class Node:
+Point = namedtuple("Point", ["x", "y"])
 
+
+class Node:
     """Node class"""
 
-    # Page/image-related stuff
-    image: np.ndarray
-    height: int
-    width: int
-    x: int
-    y: int
-
-    # Node/tree-related stuff
     parent: Optional["Node"]
-    children: list["Node"]
+    children: Sequence["Node"]
     depth: int
-    label: str
 
-    def __init__(self, parent: "Node" = None, x: int = 0, y: int = 0):
+    def __init__(self, parent: Optional["Node"] = None):
         self.parent = parent
         self.children = []
         self.depth = parent.depth + 1 if parent else 0
-        self.x = parent.x + x if parent else x
-        self.y = parent.y + y if parent else y
 
     def __getitem__(self, i):
         if isinstance(i, int):
@@ -43,21 +35,21 @@ class Node:
         i, *rest = i
         return self.children[i][rest] if rest else self.children[i]
 
-    def leaves(self):
+    def leaves(self): # -> Sequence[Self]:
         """Return the leaf nodes attached to this node"""
         nodes = [] if self.children else [self]
         for child in self.children:
             nodes.extend(child.leaves())
         return nodes
 
-    def traverse(self, filter: Optional[Callable[["Node"], bool]] = None):
+    def traverse(self, filter: Optional[Callable[["Node"], bool]] = None) -> Sequence["Node"]:
         """Return all nodes attached to this node"""
         nodes = [self] if (filter is None or filter(self)) else []
         for child in self.children:
             nodes.extend(child.traverse(filter=filter))
         return nodes
 
-    def tree2str(self, sep="", is_last=True):
+    def tree2str(self, sep: str="", is_last: bool=True) -> str:
         """Return a string representation of this node and its decendants"""
         lines = [sep + ("└──" if is_last else "├──") + str(self)]
         sep += "    " if is_last else "│   "
@@ -65,12 +57,45 @@ class Node:
             lines.append(child.tree2str(sep, child == self.children[-1]))
         return "\n".join(lines)
 
-    @singledispatchmethod
-    def update(self, result: SegmentationResult | RecognitionResult):
-        """Update this node with `result`"""
+    def is_leaf(self) -> bool:
+        return not self.children
 
-    @update.register
-    def _(self, result: SegmentationResult):
+
+class BaseDocumentNode(Node, ABC):
+    """Extension of Node class with functionality related to documents"""
+
+    height: int
+    width: int
+    coord: Point
+    label: str
+    polygon: list[tuple[int, int]]
+    bbox: tuple[int, int, int, int]
+    children: Sequence["RegionNode"]
+    text: Optional[RecognitionResult]
+
+    def __str__(self) -> str:
+        return f'{self.height}x{self.width} region ({self.label}) at ({self.coord.x}, {self.coord.y})'
+
+    @abstractproperty
+    def image(self):
+        pass
+
+    def contains_text(self) -> bool:
+        return any(child.contains_text() for child in self.children)
+
+    def has_regions(self) -> bool:
+        return all(not child.is_leaf() for child in self.children)
+
+    def update(self, result: SegmentationResult | RecognitionResult) -> None:
+        """Update this node with `result`"""
+        if isinstance(result, SegmentationResult):
+            self._register_segmentation_result(result)
+        elif isinstance(result, RecognitionResult):
+            self._register_recognition_result(result)
+        else:
+            raise TypeError(f"Incorrect type for BaseDocumentNode.update: {type(result)}")
+
+    def _register_segmentation_result(self, result: SegmentationResult) -> None:
         """Segment this node
 
         Creates children according to the segmentation and attaches them to this node.
@@ -80,136 +105,88 @@ class Node:
             children.append(RegionNode(segment, self))
         self.children = children
 
-    def is_leaf(self):
-        return not self.children
+    @abstractmethod
+    def _register_recognition_result(self, result: RecognitionResult) -> None:
+        """Register recognition result"""
+
+    def segments(self):
+        for leaf in self.leaves():
+            yield leaf.image
 
 
-class RegionNode(Node):
+class RegionNode(BaseDocumentNode):
     """A node representing a segment of a page"""
 
-    children: list["RegionNode"]
     segment: Segment
-    parent: Node
-    text: RecognitionResult
+    parent: BaseDocumentNode
 
-    def __init__(self, segment: Segment, parent: Node):
+    DEFAULT_LABEL = "region"
+
+    def __init__(self, segment: Segment, parent: BaseDocumentNode):
+        super().__init__(parent)
         self.segment = segment
         self.text = None
-        self.label = segment.class_label
-        x, _, y, _ = segment.bbox
-        super().__init__(parent, x, y)
+        self.label = segment.class_label if segment.class_label else RegionNode.DEFAULT_LABEL
+        x1, x2, y1, y2 = segment.bbox
+        self.height = y2 - y1
+        self.width = x2 - x1
+        self.polygon = [(x + parent.coord.x, y + parent.coord.y) for x, y in segment.polygon]
+        self.bbox = (x1 + parent.coord.x, x2 + parent.coord.x, y1 + parent.coord.y, y2 + parent.coord.y)
+        self.coord = Point(parent.coord.x + x1, parent.coord.y + y1)
 
-    def __str__(self):
-        s = f'{self.height}x{self.width} region at ({self.x}, {self.y})'
+    def __str__(self) -> str:
         if self.text:
-            s += f' "{self.text.top_candidate()}"'
-        return s
+            return f'{super().__str__()}: "{self.text.top_candidate()}"'
+        return super().__str__()
 
     @property
     def image(self):
         """The image this segment represents"""
-        bbox = self.segment.bbox
-        mask = self.segment.mask
-        return image.mask(image.crop(self.parent.image, bbox), mask)
+        img = image.crop(self.parent.image, self.segment.bbox)
+        if self.segment.mask is not None:
+            img = image.mask(img, self.segment.mask)
+        return img
 
-    @property
-    def height(self) -> int:
-        """Height of region"""
-        *_, y1, y2 = self.segment.bbox
-        return y2-y1
-
-    @property
-    def width(self) -> int:
-        """Width of region"""
-        x1, x2, *_ = self.segment.bbox
-        return x2-x1
-
-    @property
-    def polygon(self):
-        """Region polygon, relative to original image size"""
-        return [(self.parent.x + x, self.parent.y + y) for x, y in self.segment.polygon]
-
-    @property
-    def bbox(self):
-        """Region bounding box, relative to original image size"""
-        x, y = self.parent.x, self.parent.y
-        x1, x2, y1, y2 = self.segment.bbox
-        return x1+x, x2+x, y1+y, y2+y
-
-    @singledispatchmethod
-    def update(self, result: SegmentationResult | RecognitionResult):
-        """Update this node with `result`"""
-        super().update(result)
-
-    @update.register
-    def _(self, result: RecognitionResult):
-        """Update the text of this node"""
+    def _register_recognition_result(self, result: RecognitionResult) -> None:
         self.text = result
 
-    def contains_text(self):
+    def contains_text(self) -> bool:
         if not self.children:
             return self.text is not None
-        return any(child.contains_text() for child in self.children)
+        return super().contains_text()
 
-    def is_region(self):
-        return self.children and not self.text
+    def is_region(self) -> bool:
+        return bool(self.children) and not self.text
 
 
-class PageNode(Node):
+class PageNode(BaseDocumentNode):
     """A node representing a page / input image"""
 
-    children: list[RegionNode]
-    image: np.ndarray
-    image_path: str
-    image_name: str
+    text = None
 
     def __init__(self, image_path: str):
-        self.image = cv2.imread(image_path)
+        self._image = cv2.imread(image_path)
         self.image_path = image_path
+        self.height, self.width = self.image.shape[:2]
+        self.coord = Point(0, 0)
+        self.polygon = [(0, 0), (0, self.height), (self.width, self.height), (self.width, 0)]
+        self.bbox = (0, self.width, 0, self.height)
+
         # Extract image name and remove file extension (`path/to/image.jpg` -> `image`)
         self.image_name = os.path.basename(image_path).split(".")[0]
         self.label = self.image_name
         super().__init__()
 
-    def __str__(self):
-        return f'{self.height}x{self.width} image {self.image_name}'
-
     @property
-    def height(self):
-        """Image height"""
-        return self.image.shape[0]
+    def image(self):
+        return self._image
 
-    @property
-    def width(self):
-        """Image width"""
-        return self.image.shape[1]
-
-    @property
-    def polygon(self):
-        return [(0, 0), (0, self.height), (self.width, self.height), (self.width, 0)]
-
-    @property
-    def bbox(self):
-        return (0, self.width, 0, self.height)
-
-    @singledispatchmethod
-    def update(self, result: RecognitionResult):
-        """Update this node with `result`"""
-        super().update(result)
-
-    @update.register
-    def _(self, result: RecognitionResult):
+    def _register_recognition_result(self, result: RecognitionResult):
         """Update the text of this node"""
         segment = Segment.from_bbox(self.bbox)
         child = RegionNode(segment, self)
         child.update(result)
         self.children = [child]
-
-    def contains_text(self):
-        return any(child.contains_text() for child in self.children)
-
-    def has_regions(self):
-        return all(not child.is_leaf() for child in self.children)
 
 
 class Volume:
@@ -217,25 +194,30 @@ class Volume:
     """Class representing a collection of input images"""
 
     def __init__(self, paths: list[str]):
-        self._root = Node()
-        self._root.children = [PageNode(path) for path in paths]
+        self.pages = [PageNode(path) for path in paths]
         self.label = "untitled_volume"
 
     def __getitem__(self, i):
-        return self._root[i]
+        return self.pages[i]
 
     def __iter__(self):
-        return self._root.children.__iter__()
+        return self.pages.__iter__()
 
     def __str__(self):
-        return self._root.tree2str()
+        return "\n".join(page.tree2str() for page in self.pages)
 
     def images(self):  # -> Generator[np.ndarray]:
         """Yields the volume's original input images"""
-        for image_node in self._root.children:
-            yield image_node.image
+        for page in self.pages:
+            yield page.image
 
-    def segments(self, depth: int = None):  # -> Generator[np.ndarray]:
+    def leaves(self): # -> Iterable[BaseDocumentNode]:
+        return chain.from_iterable(page.leaves() for page in self.pages)
+
+    def traverse(self):
+        return chain.from_iterable(page.traverse() for page in self.pages)
+
+    def segments(self, depth: Optional[int] = None): # -> Iterable[np.ndarray]:
         """Yields the volume's segments at `depth`
 
         Args:
@@ -244,16 +226,22 @@ class Volume:
         """
 
         if depth is None:
-            for segment in self._root.leaves():
-                yield segment.image
+            for node in self.leaves():
+                yield node.image
         else:
-            for segment in self._root.traverse():
-                if segment.depth == depth:
-                    yield segment.image
+            for page in self.pages:
+                for node in page.traverse(lambda node: node.depth == depth):
+                    yield node.image
 
-    def update(self, results: list[SegmentationResult | RecognitionResult]):
-        """Update the volume with segmentation or text recognition results"""
-        leaves = self._root.leaves()
+    def update(self, results: list[SegmentationResult | RecognitionResult]) -> None:
+        """
+        Update the volume with model results
+
+        Arguments:
+            results: A list of results where the i:th result corresponds
+                to the volume's i:th leaf node.
+        """
+        leaves = list(self.leaves())
         if len(leaves) != len(results):
             raise ValueError(f"Size of input ({len(results)}) does not match "
                              f"the size of the tree ({len(leaves)})")
@@ -261,9 +249,7 @@ class Volume:
         for node, result in zip(leaves, results):
             node.update(result)
 
-        return self.segments()
-
-    def save(self, directory='outputs', format_: Literal['alto', 'page', 'txt'] = 'alto'):
+    def save(self, directory: str="outputs", format_: Literal["alto", "page", "txt"] = "alto") -> None:
         """Save volume
 
         Arguments:

src/htrflow_core/templates/alto
@@ -40,14 +40,14 @@
             <PrintSpace>
                 {%- for node in page.traverse() %}
                 {%- if is_text_block(node) %}
-                <TextBlock ID="{{ labels[node] }}" HPOS="{{ node.x }}" VPOS="{{ node.y }}" HEIGHT="{{ node.height}}" WIDTH="{{ node.width }}">
+                <TextBlock ID="{{ labels[node] }}" HPOS="{{ node.coord.x }}" VPOS="{{ node.coord.y }}" HEIGHT="{{ node.height}}" WIDTH="{{ node.width }}">
                     {%- if node.polygon %}
                     <Shape>
                         <Polygon POINTS="{% for point in node.polygon %}{{ point|join(',') }}{% if not loop.last %} {% endif %}{% endfor %}"/>
                     </Shape>
                     {%- endif %}
                     {%- for line in node.children %}
-                    <TextLine ID="{{ labels[line] }}"  HPOS="{{ line.x }}" VPOS="{{ line.y }}" HEIGHT="{{ line.height}}" WIDTH="{{ line.width }}">
+                    <TextLine ID="{{ labels[line] }}"  HPOS="{{ line.coord.x }}" VPOS="{{ line.coord.y }}" HEIGHT="{{ line.height}}" WIDTH="{{ line.width }}">
                         <String CONTENT="{{line.text.top_candidate()}}" />
                     </TextLine>
                     {%- endfor %}

tests/unit/test_serialization.py
@@ -33,11 +33,13 @@ def demo_page_unsegmented(demo_image):
 def demo_page_segmented_once(demo_image):
     node = volume.PageNode(demo_image)
     model = SegmentationModel()
-    result = model([node.image])
-    node.update(result[0])
+    results = model(node.segments())
+    for result, leaf in zip(results, node.leaves()):
+        leaf.update(result)
     model = RecognitionModel()
-    result = model([node.image for node in node.leaves()])
-    node.update(result[0])
+    results = model(node.segments())
+    for result, leaf in zip(results, node.leaves()):
+        leaf.update(result)
     return node
 
 
@@ -45,12 +47,14 @@ def demo_page_segmented_once(demo_image):
 def demo_page_segmented_twice(demo_image):
     node = volume.PageNode(demo_image)
     model = SegmentationModel()
-    result = model([node.image])
-    node.update(result[0])
-    model = RecognitionModel()
     for _ in range(2):
-        result = model([node.image for node in node.leaves()])
-        node.update(result[0])
+        results = model(node.segments())
+        for result, leaf in zip(results, node.leaves()):
+            leaf.update(result)
+    model = RecognitionModel()
+    results = model(node.segments())
+    for result, leaf in zip(results, node.leaves()):
+        leaf.update(result)
     return node
 
 
@@ -58,12 +62,14 @@ def demo_page_segmented_twice(demo_image):
 def demo_page_segmented_thrice(demo_image):
     node = volume.PageNode(demo_image)
     model = SegmentationModel()
-    result = model([node.image])
-    node.update(result[0])
-    model = RecognitionModel()
     for _ in range(3):
-        result = model([node.image for node in node.leaves()])
-        node.update(result[0])
+        results = model(node.segments())
+        for result, leaf in zip(results, node.leaves()):
+            leaf.update(result)
+    model = RecognitionModel()
+    results = model(node.segments())
+    for result, leaf in zip(results, node.leaves()):
+        leaf.update(result)
     return node
 
 

tests/unit/test_volume.py
@@ -4,25 +4,24 @@ import pytest
 
 import htrflow_core.volume as volume
 from htrflow_core.dummies.dummy_models import RecognitionModel, SegmentationModel
-from htrflow_core.image import read
 
 
 @pytest.fixture
 def demo_image():
-    return read('data/demo_image.jpg')
+    return 'data/demo_image.jpg'
 
 
 @pytest.fixture
-def demo_volume_unsegmented():
+def demo_volume_unsegmented(demo_image):
     n_images = 5
-    vol = volume.Volume(['data/demo_image.jpg'] * n_images)
+    vol = volume.Volume([demo_image] * n_images)
     return vol
 
 
 @pytest.fixture
-def demo_volume_segmented():
+def demo_volume_segmented(demo_image):
     n_images = 5
-    vol = volume.Volume(['data/demo_image.jpg'] * n_images)
+    vol = volume.Volume([demo_image] * n_images)
     model = SegmentationModel()
     result = model(vol.images())
     vol.update(result)
@@ -30,9 +29,9 @@ def demo_volume_segmented():
 
 
 @pytest.fixture
-def demo_volume_segmented_nested():
+def demo_volume_segmented_nested(demo_image):
     n_images = 5
-    vol = volume.Volume(['data/demo_image.jpg'] * n_images)
+    vol = volume.Volume([demo_image] * n_images)
     model = SegmentationModel()
     result = model(vol.images())
     vol.update(result)
@@ -42,9 +41,9 @@ def demo_volume_segmented_nested():
 
 
 @pytest.fixture
-def demo_volume_with_text():
+def demo_volume_with_text(demo_image):
     n_images = 1
-    vol = volume.Volume(['data/demo_image.jpg'] * n_images)
+    vol = volume.Volume([demo_image] * n_images)
     model = RecognitionModel()
     result = model(vol.images())
     vol.update(result)
@@ -97,24 +96,24 @@ def test_traverse_grandchildren():
     assert {*root.traverse()} == {root, *children, *grandchildren}
 
 
-def test_update_wrong_type():
-    root = volume.Node()
-    root.update(5)
-    # Nothing should happen
+def test_update_wrong_type(demo_image):
+    root = volume.PageNode(demo_image)
+    with pytest.raises(TypeError) as _:
+        root.update(5)
 
 
 def test_update_segmentation(demo_image):
     segmentation_model = SegmentationModel("mask")
-    result, *_ = segmentation_model([demo_image])
-    node = volume.Node()
+    node = volume.PageNode(demo_image)
+    result, *_ = segmentation_model([node.image])
     node.update(result)
     assert len(node.children) == len(result.segments)
 
 
 def test_update_segmentation_bbox(demo_image):
     segmentation_model = SegmentationModel("mask")
-    result, *_ = segmentation_model([demo_image])
-    node = volume.Node()
+    node = volume.PageNode(demo_image)
+    result, *_ = segmentation_model([node.image])
     node.update(result)
     segment_index = 0
     segment_bbox = result.segments[segment_index].bbox
@@ -124,8 +123,8 @@ def test_update_segmentation_bbox(demo_image):
 
 def test_update_segmentation_width_height(demo_image):
     segmentation_model = SegmentationModel("mask")
-    result, *_ = segmentation_model([demo_image])
-    node = volume.Node()
+    node = volume.PageNode(demo_image)
+    result, *_ = segmentation_model([node.image])
     node.update(result)
     segment_index = 0
     x1, x2, y1, y2 = result.segments[segment_index].bbox
@@ -137,14 +136,14 @@ def test_update_nested_segmentation_coordinates(demo_volume_segmented_nested):
     page = demo_volume_segmented_nested[0]
     segment = page[0]
     nested_segment = page[0, 0]
-    parent_x = segment.x
+    parent_x = segment.coord.x
     nested_segment_x_relative_to_parent = nested_segment.segment.bbox[0]
-    assert nested_segment.x == parent_x + nested_segment_x_relative_to_parent
+    assert nested_segment.coord.x == parent_x + nested_segment_x_relative_to_parent
 
-    parent_y = segment.y
+    parent_y = segment.coord.y
     nested_segment = page[0, 0]
     nested_segment_y_relative_to_parent = nested_segment.segment.bbox[2]
-    assert nested_segment.y == parent_y + nested_segment_y_relative_to_parent
+    assert nested_segment.coord.y == parent_y + nested_segment_y_relative_to_parent
 
 
 def test_update_region_text(demo_volume_segmented):
@@ -175,13 +174,14 @@ def test_polygon_nested(demo_volume_segmented_nested):
     nested_node = page[0, 0]
     # .polygon attribute should be relative to original image
     # but segment.polygon should be relative to parent
-    expected_polygon = [(node.x + x, node.y + y) for x, y in nested_node.segment.polygon]
+    expected_polygon = [(node.coord.x + x, node.coord.y + y) for x, y in nested_node.segment.polygon]
     assert nested_node.polygon == expected_polygon
 
 
 def test_polygon_not_nested(demo_volume_segmented):
-    page = demo_volume_segmented[0]
-    node = page[0]
+    ...
+    # page = demo_volume_segmented[0]
+    # node = page[0]
     # TODO fix so that the types match
     # assert node.polygon == node.segment.polygon
 
@@ -204,19 +204,19 @@ def test_volume_update_wrong_size(demo_volume_segmented):
 
 def test_volume_iter(demo_volume_segmented):
     # iterating over the volume should iterate over its children (pages)
-    assert all(a == b for a, b in zip(demo_volume_segmented, demo_volume_segmented._root.children))
+    assert all(a == b for a, b in zip(demo_volume_segmented, demo_volume_segmented.pages))
 
 
 def test_volume_segments_depth(demo_volume_segmented):
     depth = 1
-    all_nodes = demo_volume_segmented._root.traverse()
+    all_nodes = demo_volume_segmented.traverse()
     expected_n_images = sum(node.depth==depth for node in all_nodes)
     n_images = len([*demo_volume_segmented.segments(depth=depth)])
     assert n_images == expected_n_images
 
 
 def test_volume_segments_depth_none(demo_volume_segmented):
-    leaves = demo_volume_segmented._root.leaves()
+    leaves = demo_volume_segmented.leaves()
     segments = demo_volume_segmented.segments()
     # (A==B).all() checks if two arrays are equal (cannot do A==B)
     assert all((img == leaf.image).all() for img, leaf in zip(segments, leaves))

commit ed3e1c324c0418e3b54ae93185535c0f3ef5144f

Restore tuple indexing of Volume
Commit 5873af2 broke tuple indexing of volumes by getting rid of the
root nodes.

src/htrflow_core/volume.py
@@ -247,6 +247,9 @@ class Volume:
         return path
 
     def __getitem__(self, i):
+        if isinstance(i, Iterable):
+            i, *rest = i
+            return self.pages[i].__getitem__(rest)
         return self.pages[i]
 
     def __iter__(self):