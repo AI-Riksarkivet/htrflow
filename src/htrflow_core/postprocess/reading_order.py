bab4f04a598b57650c0aa66d8d0c2adbaf3fac00
viklofg <viktoria.lofgren@riksarkivet.se>
2024-03-21 09:21:36 +0000

    Remove pandas dependency from reading order
    Rewrote the RO functions in pure python + numpy to remove HTRFLOW's
    dependency of pandas. The new order_segments_marginalia (should) also
    find marginalia along the binding in spreads. Otherwise no changes to
    the logic. No tests yet.

--- src/htrflow_core/postprocess/reading_order.py
+++ src/htrflow_core/postprocess/reading_order.py
@@ -1,60 +1,70 @@
 from typing import Sequence
 
 import numpy as np
-import pandas as pd
 
 from htrflow_core.results import Result
+from htrflow_core.utils.geometry import Bbox
 
 
-def order_segments_marginalia(result: Result, margin_ratio=0.2, histogram_bins=50, histogram_dip_ratio=0.5):
-    # Adapted from htrflow_core/src/transformations/order_segments.py
+def is_margin(region: Bbox, page: Bbox, margin_ratio: float=0.2):
+    """Check if `region` lies in the (left or right) margin of `page`
 
-    # Create a pandas DataFrame from the regions
-    df = pd.DataFrame(result.bboxes(), columns=["x_min", "x_max", "y_min", "y_max"])
+    The region is assumed to be marginalia if its center point on the
+    x-axis lies close to the left or right side of the page.
 
-    # Calculate the centroids of the bounding boxes
-    df["centroid_x"] = (df["x_min"] + df["x_max"]) / 2
-    df["centroid_y"] = (df["y_min"] + df["y_max"]) / 2
+    Arguments:
+        region: The region to check
+        page: The boundaries of the page that the region lies in
+        margin_ratio: Margin to page width ratio. Default is 0.2.
+    """
+    margin = page.width * margin_ratio
+    left_margin = page.x1 + margin
+    right_margin = page.x2 - margin
+    return (region.center.x < left_margin or region.center.x > right_margin)
+
+
+def is_twopage(segments: Sequence[Bbox], histogram_bins=50, histogram_dip_ratio=0.5):
+    xs = [segment.center.x for segment in segments]
 
     # Calculate a histogram of the x-coordinates of the centroids
-    histogram, _ = np.histogram(df["centroid_x"], bins=histogram_bins)
+    histogram, _ = np.histogram(xs, bins=histogram_bins)
 
     # Determine if there's a significant dip in the histogram, which would suggest a two-page layout
-    is_two_pages = np.min(histogram) < np.max(histogram) * histogram_dip_ratio
+    return np.min(histogram) < np.max(histogram) * histogram_dip_ratio
 
-    image_width = result.image.shape[1]
 
-    if is_two_pages:
-        # Determine which page each region is on
+def order_segments_marginalia(result: Result, histogram_bins=50, histogram_dip_ratio=0.5):
+    # Adapted from htrflow_core/src/transformations/order_segments.py
+
+    xs = [segment.center.x for segment in result.segments]
+    ys = [segment.center.y for segment in result.segments]
+
+    image_height, image_width = result.image.shape[:2]
+    index = list(range(len(result.segments)))
+
+    if is_twopage(result.segments, histogram_bins, histogram_dip_ratio):
         page_width = int(image_width / 2)
-        df["page"] = (df["centroid_x"] > page_width).astype(int)
-
-        # Determine if the region is in the margin
-        margin_width = page_width * margin_ratio
-        df["is_margin"] = ((df["page"] == 0) & (df["centroid_x"] < margin_width)) | (
-            (df["page"] == 1) & (df["centroid_x"] > image_width - margin_width)
-        )
+        pagei = [x < page_width for x in xs]
+        pages = [Bbox(0, page_width, 0, image_height), Bbox(page_width, image_width, 0, image_height)]
+        index.sort(key=lambda i: (pagei[i], is_margin(result.segments[i], pages[pagei[i]], ys[i], xs[i])))
     else:
-        df["page"] = 0
-        df["is_margin"] = (df["centroid_x"] < image_width * margin_ratio) | (
-            df["centroid_x"] > image_width - page_width * margin_ratio
-        )
+        page = Bbox(0, image_width, 0, image_height)
+        index.sort(key=lambda i: (is_margin(result.segments[i], page), ys[i], xs[i]))
 
-    df = df.sort_values(by=["page", "is_margin", "centroid_y", "centroid_x"])
-    return df.index.tolist()
+    return index
 
 
 def order_lines(result: Result, line_spacing_factor=0.5) -> Sequence[int]:
     # Adapted from htrflow_core/src/transformations/order_segments.py
 
-    centroid_x = [(x1 + x2) / 2 for x1, x2, *_ in result.bboxes()]
-    centroid_y = [(y1 + y2) / 2 for *_, y1, y2 in result.bboxes()]
+    xs = [segment.center.x for segment in result.segments]
+    ys = [segment.center.y for segment in result.segments]
 
     # Calculate the threshold distance
-    average_line_height = np.mean([y2 - y1 for *_, y1, y2 in result.bboxes()])
+    average_line_height = sum(segment.height for segment in result.segments) / len(result.segments)
     threshold_distance = average_line_height * line_spacing_factor
 
     # Sort the indices based on vertical center points and horizontal positions
     index = list(range(len(result.segments)))
-    index.sort(key=lambda i: (centroid_x[i] // threshold_distance, centroid_y[i]))
+    index.sort(key=lambda i: (xs[i] // threshold_distance, ys[i]))
     return index
