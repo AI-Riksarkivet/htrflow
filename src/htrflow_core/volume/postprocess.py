023748c4f83c663c53c1892b4996703aa54b1999
viklofg <viktoria.lofgren@riksarkivet.se>
2024-04-05 11:29:35 +0200

    Add layout module
    Moved recent days' layout related work to its own module.

--- src/htrflow_core/volume/postprocess.py
+++ src/htrflow_core/volume/postprocess.py
@@ -1,6 +1,5 @@
 from copy import deepcopy
 
-from htrflow_core.utils.geometry import estimate_printspace, get_region_location
 from htrflow_core.volume import volume
 
 
@@ -44,22 +43,3 @@ def is_noise(node: volume.BaseDocumentNode, threshold: float = 0.8):
         conf = sum(child.get("text_result").top_score() for child in node) / len(node.children)
         return conf < threshold
     return False
-
-
-def label_regions(volume: volume.Volume, key="region_location"):
-    """Label volume's regions
-
-    Labels each top-level segment of the volume as one of the five
-    region types specified by geometry.RegionLocation. Saves the label
-    in the node's data dictionary under `key`.
-
-    Arguments:
-        volume: Input volume
-        key: Key used to save the region label. Defaults to
-            "region_location".
-    """
-
-    for page in volume:
-        printspace = estimate_printspace(page.image)
-        for node in page:
-            node.add_data(**{key: get_region_location(printspace, node.bbox)})
