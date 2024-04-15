Fix off-by-one bug in cropping
Endpoints x2 and y2 should be included in the cropped image => increase
slice by one

src/htrflow_core/utils/imgproc.py
@@ -21,7 +21,7 @@ def crop(image: np.ndarray, bbox: Bbox) -> np.ndarray:
         bbox: The bounding box
     """
     x1, y1, x2, y2 = bbox
-    return image[y1:y2, x1:x2]
+    return image[y1:y2+1, x1:x2+1]
 
 
 def mask(