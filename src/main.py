5fc51d6e1fe246af39af0d01ec3231b0cb06ef6d
viklofg <viktoria.lofgren@riksarkivet.se>
2024-04-16 06:57:53 +0000

    Use argparse in main

--- src/main.py
+++ src/main.py
@@ -7,8 +7,8 @@ Usage:
 Example:
 > python src/main.py data/pipelines/demo.yaml data/demo_images/A0068699
 """
+import argparse
 import logging
-import sys
 
 import yaml
 
@@ -17,13 +17,23 @@ from htrflow_core.pipeline.steps import auto_import
 from htrflow_core.serialization import get_serializer
 
 
-logging.basicConfig(filename='htrflow.log', level=logging.INFO, filemode='w')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="HTRFLOW")
+    parser.add_argument("pipeline")
+    parser.add_argument("input")
+    parser.add_argument("-o", "--output", default="outputs")
+    parser.add_argument("--logfile", default="htrflow.log")
+    parser.add_argument("--loglevel", choices=["debug", "info", "warning", "error"], default="info")
+    args = parser.parse_args()
 
-if __name__ == '__main__':
-    with open(sys.argv[1], 'r') as f:
+    logging.basicConfig(filename=args.logfile, level=args.loglevel, filemode="w")
+
+    with open(args.pipeline, "r") as f:
         config = yaml.safe_load(f)
 
     pipe = Pipeline.from_config(config)
-    volume = auto_import(sys.argv[2])
+    volume = auto_import(args.input)
     volume = pipe.run(volume)
-    volume.save(serializer=get_serializer(config["export"]["format"], **config["export"].get("settings", {})))
+    volume.save(
+        args.output, serializer=get_serializer(config["export"]["format"], **config["export"].get("settings", {}))
+    )
