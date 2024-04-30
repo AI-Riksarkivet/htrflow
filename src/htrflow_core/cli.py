f0e38680441ac70a4c5369540e59a264bfb696a4
Borg93 <gabriel.borg@riksarkivet.se>
2024-04-30 16:05:54 +0200

    fixed cli to work for poetry build
    still needs to fix typer...

--- src/htrflow_core/cli.py
+++ src/htrflow_core/cli.py
@@ -1,5 +1,6 @@
 import logging
 
+import cowsay
 import typer
 import yaml
 
@@ -7,7 +8,7 @@ from htrflow_core.pipeline.pipeline import Pipeline
 from htrflow_core.pipeline.steps import auto_import
 
 
-app = typer.Typer()
+app = typer.Typer(name="htrflow_core", add_completion=False, help="CLI inferface for htrflow_core's pipeline")
 
 
 def setup_pipeline_logging(logfile: str, loglevel: str):
@@ -21,24 +22,34 @@ def setup_pipeline_logging(logfile: str, loglevel: str):
     logger.addHandler(handler)
 
 
-@app.command()
+@app.command("pipeline-cli")
 def main(
     pipeline: str = typer.Argument(..., help="Path to the pipeline configuration YAML file"),
-    input_dirs: list = typer.Argument(..., help="Input directory or directories"),
+    input_dirs: str = typer.Argument(..., help="Input directory or directories"),
     logfile: str = typer.Option(None, "--logfile", help="Log file path"),
     loglevel: str = typer.Option("info", "--loglevel", help="Logging level", case_sensitive=False),
 ):
-    """Entrypoint for HTRFLOW."""
+    """Entrypoint for htrflow_core's pipeline."""
     setup_pipeline_logging(logfile, loglevel.upper())
     try:
         with open(pipeline, "r") as file:
             config = yaml.safe_load(file)
+
         pipe = Pipeline.from_config(config)
+
         volume = auto_import(input_dirs)
+
+        typer.echo("Running Pipeline")
         volume = pipe.run(volume)
     except Exception as e:
         typer.echo(f"Error: {e}", err=True)
         raise typer.Exit(code=1)
+
+
+@app.command()
+def test(msg: str = "Hello World"):
+    """Test CLI with cowsay"""
+    typer.echo(cowsay.get_output_string("cow", msg))
 
 
 if __name__ == "__main__":
