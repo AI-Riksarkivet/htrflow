f3a9aba9719c67cc339989b48e9944fc87b5ef1f
viklofg <viktoria.lofgren@riksarkivet.se>
2024-06-04 11:26:27 +0200

    Update satrn model
    Remove redundant code and unused arguments, update to use new
    text_recognition_result.

--- src/htrflow_core/models/openmmlab/satrn.py
+++ src/htrflow_core/models/openmmlab/satrn.py
@@ -1,35 +1,28 @@
 import logging
 
-import numpy as np
 from mmocr.apis import TextRecInferencer
-from mmocr.structures import TextRecogDataSample
 
 from htrflow_core.models.base_model import BaseModel
 from htrflow_core.models.enums import Framework, Task
 from htrflow_core.models.hf_utils import MMLabsDownloader
 from htrflow_core.models.mixins.torch_mixin import PytorchMixin
 from htrflow_core.models.openmmlab.utils import SuppressOutput
-from htrflow_core.results import RecognizedText, Result
+from htrflow_core.results import Result
+from htrflow_core.utils.imgproc import NumpyImage
 
 
 logger = logging.getLogger(__name__)
 
 
 class Satrn(BaseModel, PytorchMixin):
-    def __init__(
-        self,
-        model: str = "Riksarkivet/satrn_htr",
-        config: str = "Riksarkivet/satrn_htr",
-        *model_args,
-        **kwargs,
-    ) -> None:
-        super().__init__(**kwargs)
+    def __init__(self, model: str, config: str | None = None, device: str | None = None) -> None:
+        super().__init__(device)
 
-        model_weights, model_config = MMLabsDownloader.from_pretrained(model, config, self.cache_dir)
+        model_weights, model_config = MMLabsDownloader.from_pretrained(model, config)
 
         with SuppressOutput():
             self.model = TextRecInferencer(
-                model=model_config, weights=model_weights, device=self.set_device(self.device), *model_args
+                model=model_config, weights=model_weights, device=self.set_device(self.device)
             )
 
         logger.info(f"Model loaded on ({self.device_id}) from {model}.")
@@ -40,22 +33,14 @@ class Satrn(BaseModel, PytorchMixin):
                 "config": str(config),
                 "framework": Framework.Openmmlab.value,
                 "task": Task.Image2Text.value,
-                "device": self.device_id,
             }
         )
 
-    def _predict(self, images: list[np.ndarray], **kwargs) -> list[Result]:
-        if len(images) > 1:
-            batch_size = len(images)
-        else:
-            batch_size = 1
-
-        outputs: TextRecogDataSample = self.model(
-            images, batch_size=batch_size, return_datasamples=False, progress_bar=False, **kwargs
-        )
-
-        return [self._create_text_result(output) for output in outputs["predictions"]]
-
-    def _create_text_result(self, output: list) -> Result:
-        recognized_text = RecognizedText(texts=[output["text"]], scores=[output["scores"]])
-        return Result.text_recognition_result(metadata=self.metadata, text=recognized_text)
+    def _predict(self, images: list[NumpyImage], **kwargs) -> list[Result]:
+        outputs = self.model(images, batch_size=len(images), return_datasamples=False, progress_bar=False, **kwargs)
+        results = []
+        for prediction in outputs["predictions"]:
+            texts = prediction["text"]
+            scores = prediction["scores"]
+            results.append(Result.text_recognition_result(self.metadata, texts, scores))
+        return results
