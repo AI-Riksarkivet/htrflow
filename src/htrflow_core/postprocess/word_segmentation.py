e4a287f09162c09c8a97e077248d199cc0b640db
viklofg <viktoria.lofgren@riksarkivet.se>
2024-06-17 16:09:39 +0200

    Fix bug in old word segmentation

--- src/htrflow_core/postprocess/word_segmentation.py
+++ src/htrflow_core/postprocess/word_segmentation.py
@@ -1,4 +1,6 @@
 from htrflow_core.results import RecognizedText, Result, Segment
+from htrflow_core.utils.geometry import bbox2mask
+from htrflow_core.utils.imgproc import mask
 from htrflow_core.volume.volume import SegmentNode
 
 
@@ -11,7 +13,8 @@ def _simple_word_segmentation(node: SegmentNode):
     for word in words:
         x2 = min(x1 + pixels_per_char * (len(word) + 1), node.width)
         bboxes.append((x1, 0, x2, node.height))
-    segments = [Segment(bbox=bbox, class_label="word") for bbox in bboxes]
+        x1 = x2
+    segments = [Segment(mask=mask(node.mask, bbox2mask(bbox, node.mask.shape), fill=0), class_label="word") for bbox in bboxes]
     texts = [RecognizedText([word], [0]) for word in words]
     return Result({}, segments=segments, texts=texts)
 
