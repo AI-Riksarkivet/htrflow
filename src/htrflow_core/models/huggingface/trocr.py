321825303a9e6c3b51ff871bb4ece4bad82fa84c
viklofg <viktoria.lofgren@riksarkivet.se>
2024-06-26 08:41:26 +0200

    Update TrOCR documentation and structure

--- src/htrflow_core/models/huggingface/trocr.py
+++ src/htrflow_core/models/huggingface/trocr.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Any
 
 import numpy as np
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
@@ -16,41 +17,48 @@ logger = logging.getLogger(__name__)
 
 
 class TrOCR(BaseModel, PytorchMixin):
-    default_generation_kwargs = {
-        "num_beams": 4,
-    }
+    """
+    HTRFLOW adapter of the tranformer-based OCR model TrOCR.
+
+    Uses huggingface's implementation of TrOCR. For further
+    information, see
+    https://huggingface.co/docs/transformers/model_doc/trocr.
+    """
 
     def __init__(
         self,
-        model: str = "microsoft/trocr-base-handwritten",
-        processor: str = "microsoft/trocr-base-handwritten",
-        model_kwargs: dict | None = None,
-        processor_kwargs: dict | None = None,
+        model: str,
+        processor: str | None = None,
+        model_kwargs: dict[str, Any] | None = None,
+        processor_kwargs: dict[str, Any] | None = None,
         **kwargs,
     ):
         """Initialize a TrOCR model
 
         Arguments:
             model: Path or name of pretrained VisisonEncoderDeocderModel.
-                Defaults to 'microsoft/trocr-base-handwritten'.
-            processor: Path or name of pretrained TrOCRProcessor.
-                Defaults to 'microsoft/trocr-base-handwritten'.
+            processor: Optional path or name of pretrained TrOCRProcessor.
+                If not given, the model path or name is used.
+            model_kwargs: Model initialization kwargs which are forwarded to
+                VisionEncoderDecoderModel.from_pretrained.
+            processor_kwargs: Processor initialization kwargs which are
+                forwarded to TrOCRProcessor.from_pretrained.
+            kwargs: Additional kwargs which are forwarded to BaseModel's
+                __init__.
         """
         super().__init__(**kwargs)
+
+        # Initialize model
         model_kwargs = HF_CONFIG | (model_kwargs or {})
         self.model = VisionEncoderDecoderModel.from_pretrained(model, **model_kwargs)
         self.model.to(self.set_device(self.device))
-        logger.info(
-            "Initialized TrOCR model from %s on device %s. Initialization parameters: %s",
-            model,
-            self.model.device,
-            model_kwargs,
-        )
+        logger.info("Initialized TrOCR model from %s on device %s.", model, self.model.device)
 
+        # Initialize processor
         processor = processor or model
         processor_kwargs = HF_CONFIG | (processor_kwargs or {})
         self.processor = TrOCRProcessor.from_pretrained(processor, **processor_kwargs)
-        logger.info("Initialized TrOCR processor from %s. Initialization parameters: %s", processor, processor_kwargs)
+        logger.info("Initialized TrOCR processor from %s.", processor)
 
         self.metadata.update(
             {
@@ -62,16 +70,8 @@ class TrOCR(BaseModel, PytorchMixin):
             }
         )
 
-    def filter_text_on_thresh():
-        # TODO: Should add **kwargs: that also filter output.
-        # For instance pred_threshold = 0.7 should filter conf score.
-        pass
-
     def _predict(self, images: list[np.ndarray], **generation_kwargs) -> list[Result]:
         """Perform inference on `images`
-
-        Uses beam search with 4 beams by default, this can be altered
-        by setting `num_beams` in generation_kwargs.
 
         Arguments:
             images: Input images.
@@ -79,28 +79,30 @@ class TrOCR(BaseModel, PytorchMixin):
                 forwarded to the model's .generate() method.
 
         Returns:
-            The predicted texts and their beam scores.
+            The predicted texts and confidence scores as a list of `Result` instances.
         """
-        # Prepare generation keyword arguments
-        generation_kwargs = self._prepare_generation_kwargs(**generation_kwargs)
-        metadata = self.metadata | ({"generation_args": generation_kwargs} if generation_kwargs else {})
 
+        # Prepare generation keyword arguments: Generally, all kwargs are
+        # forwarded to the model's .generate method, but some need to be
+        # explicitly set (and possibly overridden) to ensure that we get the
+        # output format we want.
+        generation_kwargs["num_return_sequences"] = generation_kwargs.get("num_beams", 1)
+        generation_kwargs["output_scores"] = True
+        generation_kwargs["return_dict_in_generate"] = True
+
+        # Do inference
         model_inputs = self.processor(images, return_tensors="pt").pixel_values
         model_outputs = self.model.generate(model_inputs.to(self.model.device), **generation_kwargs)
 
         texts = self.processor.batch_decode(model_outputs.sequences, skip_special_tokens=True)
+        scores = self._compute_seuqence_scores(model_outputs)
 
-        scores = self.compute_seuqence_scores(model_outputs)
-        step = generation_kwargs["num_return_sequences"]
-
-        return self._create_text_results(texts, scores, metadata, step)
-
-    def _create_text_results(self, texts: list[str], scores: list[float], metadata: dict, step: int) -> list[Result]:
-        """Assemble and return a list of Result objects from the prediction outputs.
-        `texts` and `scores` are flattened lists so we need to iterate over them in steps.
-        This is done to ensure that the list of results correspond 1-to-1 with the list of images.
-        """
+        # Assemble and return a list of Result objects from the prediction outputs.
+        # `texts` and `scores` are flattened lists so we need to iterate over them in steps.
+        # This is done to ensure that the list of results correspond 1-to-1 with the list of images.
         results = []
+        metadata = self.metadata | {"generation_kwargs": generation_kwargs}
+        step = generation_kwargs["num_return_sequences"]
         for i in range(0, len(texts), step):
             texts_chunk = texts[i : i + step]
             scores_chunk = scores[i : i + step]
@@ -108,23 +110,7 @@ class TrOCR(BaseModel, PytorchMixin):
             results.append(result)
         return results
 
-    def _prepare_generation_kwargs(self, **kwargs):
-        """
-        Generally, all generation keyword arguments are passed to
-        the model's .generate method. However, to ensure that we
-        get the output format we want, some arguments needs to be
-        set (and potentially overridden).
-        """
-        # Add default arguments
-        kwargs = TrOCR.default_generation_kwargs | kwargs
-        kwargs["num_return_sequences"] = kwargs.get("num_beams", 1)
-
-        # Override arguments related to the output format
-        kwargs["output_scores"] = True
-        kwargs["return_dict_in_generate"] = True
-        return kwargs
-
-    def compute_seuqence_scores(self, outputs: ModelOutput):
+    def _compute_seuqence_scores(self, outputs: ModelOutput):
         """Compute normalized prediction score for each output sequence
 
         This function computes the normalized sequence scores from the output.
