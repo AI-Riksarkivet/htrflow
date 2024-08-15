56d70ad9e6d8aa38893ae3f46fa90f40311de195
viklofg <viktoria.lofgren@riksarkivet.se>
2024-08-15 14:20:00 +0200

    Use mmdet's mask_matrix_nms in RTMDet
    It uses the regions' scores instead of their area, which means that
    small regions are kept if their score is high. Solves AB#510.

--- src/htrflow_core/models/openmmlab/rtmdet.py
+++ src/htrflow_core/models/openmmlab/rtmdet.py
@@ -1,14 +1,15 @@
 import logging
 
-import numpy as np
+import torch
+import torch.nn.functional as F
 from mmdet.apis import DetInferencer
+from mmdet.models.layers.matrix_nms import mask_matrix_nms
 from mmdet.structures import DetDataSample
 from mmengine.structures import InstanceData
 
 from htrflow_core.models.base_model import BaseModel
 from htrflow_core.models.hf_utils import commit_hash_from_path, load_mmlabs
 from htrflow_core.models.openmmlab.utils import SuppressOutput
-from htrflow_core.postprocess.mask_nms import multiclass_mask_nms
 from htrflow_core.results import Result
 from htrflow_core.utils.imgproc import NumpyImage, resize
 
@@ -44,6 +45,9 @@ class RTMDet(BaseModel):
                 repo which contains a config.py file, for example
                 'Riksarkivet/rtmdet_lines'.
             device: Model device.
+            revision: A specific model revision, as a commit hash of the
+                model's huggingface repo. If None, the latest available
+                revision is used.
         """
         super().__init__(device)
 
@@ -76,8 +80,25 @@ class RTMDet(BaseModel):
         )
 
     def _predict(
-        self, images: list[NumpyImage], nms_downscale: float = 1.0, **kwargs
+        self,
+        images: list[NumpyImage],
+        nms_downscale: float = 1.0,
+        nms_threshold: float = 0.4,
+        nms_sigma: float = 2.0,
+        **kwargs
     ) -> list[Result]:
+        """
+        Do inference.
+
+        Arguments:
+            images: List of input images
+            nms_downscale: If < 1, all masks will be downscaled by this factor
+                before applying NMS. This leads to faster NMS at the expense of
+                accuracy.
+            nms_threshold: Score threshold for segments to keep after NMS.
+            nms_sigma: NMS parameter that affects the score calculation.
+            **kwargs: Additional arguments that are passed to DetInferencer.__call__.
+        """
         batch_size = max(1, len(images))
         outputs = self.model(
             images,
@@ -89,42 +110,57 @@ class RTMDet(BaseModel):
         results = []
         for image, output in zip(images, outputs["predictions"]):
             results.append(
-                self._create_segmentation_result(image, output, nms_downscale)
+                self._create_segmentation_result(
+                    image, output, nms_downscale, nms_threshold, nms_sigma
+                )
             )
         return results
 
     def _create_segmentation_result(
-        self, image: NumpyImage, output: DetDataSample, nms_downscale: float
+        self,
+        image: NumpyImage,
+        output: DetDataSample,
+        nms_downscale: float,
+        nms_threshold: float,
+        nms_sigma: float,
     ) -> Result:
         sample: InstanceData = output.pred_instances
-        orig_shape = image.shape[:2]
+
+        # Cast masks to uint8 (needed for F.interpolate and Result obj)
+        masks = sample.masks.to(torch.uint8)
+
+        # Apply mask NMS
+        downscaled_masks = F.interpolate(masks, scale_factor=nms_downscale).to(
+            torch.bool
+        )
+        scores, labels, _, keep_inds = mask_matrix_nms(
+            downscaled_masks,
+            sample.labels,
+            sample.scores,
+            sigma=nms_sigma,
+            filter_thr=nms_threshold,
+        )
 
         # RTMDet sometimes return masks of slightly different shape (+- a few pixels)
         # than the input image. To avoid alignment problems later on, all masks are
         # resized to the original image shape.
-        np_masks = sample.masks.cpu().numpy().astype(np.uint8)
-        masks = [resize(mask, orig_shape) for mask in np_masks]
-
-        boxes = sample.bboxes.int().tolist()
-        scores = sample.scores.tolist()
-        class_labels = sample.labels.tolist()
+        orig_shape = image.shape[:2]
+        masks = [resize(mask, orig_shape) for mask in masks[keep_inds].cpu().numpy()]
 
         result = Result.segmentation_result(
             orig_shape,
-            bboxes=boxes,
+            bboxes=sample.bboxes[keep_inds].int().tolist(),
             masks=masks,
-            scores=scores,
-            labels=class_labels,
+            scores=scores.tolist(),
+            labels=labels.tolist(),
             metadata=self.metadata,
         )
-        indices_to_drop = multiclass_mask_nms(result, downscale=nms_downscale)
-        result.drop_indices(indices_to_drop)
 
         logger.info(
-            "%s (%d x %d): Found %d segments, dropped %d",
+            "%s (%d x %d): Found %d segments, kept %d after NMS",
             getattr(image, "name", "unlabelled image"),
             *orig_shape,
-            len(scores),
-            len(indices_to_drop),
+            len(sample.scores),
+            len(result.segments),
         )
         return result
